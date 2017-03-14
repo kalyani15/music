fa88fa3ea4ab25e439a659624dbd87acb887edd4
kalyani15 <kalyani.gupta15@gmail.com>
2017-03-15 01:09:35 +0530

    New css changes

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller('myMainController', ['$scope', '$http'] function($scope, $http){
-  $http.get('data/data.json').success(function(response){
+myApp.controller('myMainController', ['$scope', '$http', function($scope, $http){
+  $http.get('data/data.json').then(function(response){
     $scope.musicPlay = response.data;
-
-  )};
-});
-
-myApp.config(){
-
-};
+  })
+    $scope.slides = [
+    {
+      image: 'http://lorempixel.com/400/200/abstract/1/cc/'
+    },
+  
+  ];
+}]);
