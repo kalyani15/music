d6924cd20971f7430d15d6dc760b4a6a901950f8
unknown <kalyani.gupta@M5-D-CNDXVS1.dir.svc.accenture.com>
2017-03-16 18:16:45 +0530

    Added new css changes

--- app/app.js
+++ app/app.js
@@ -4,10 +4,5 @@ myApp.controller('myMainController', ['$scope', '$http', function($scope, $http)
   $http.get('data/data.json').then(function(response){
     $scope.musicPlay = response.data;
   })
-    $scope.slides = [
-    {
-      image: 'http://lorempixel.com/400/200/abstract/1/cc/'
-    },
-  
-  ];
+
 }]);
