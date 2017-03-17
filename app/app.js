e8660097ce4e5823ac94d90151fd448a0eca8c8f
unknown <kalyani.gupta@M5-D-CNDXVS1.dir.svc.accenture.com>
2017-03-17 18:22:53 +0530

    Views commit

--- app/app.js
+++ app/app.js
@@ -1,8 +1,15 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ['ngRoute']);
+
+myApp.config('$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/artist', {
+    templateUrl: 'views/artist.html'
+  });
+});
 
 myApp.controller('myMainController', ['$scope', '$http', function($scope, $http){
   $http.get('data/data.json').then(function(response){
     $scope.musicPlay = response.data;
-  })
+  });
 
 }]);
