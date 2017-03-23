2e0319a4e6487b2250821c3b63856f6f461b9ee2
unknown <kalyani.gupta@M5-D-CNDXVS1.dir.svc.accenture.com>
2017-03-23 13:55:42 +0530

    Different views commit

--- app/app.js
+++ app/app.js
@@ -1,15 +1,28 @@
-var myApp = angular.module("myApp", ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config('$routeProvider', function($routeProvider){
-  $routeProvider
-  .when('/artist', {
+myApp.config(['$routeProvider',function($routeProvider){
+  $routeProvider.when('/artist',{
     templateUrl: 'views/artist.html'
-  });
-});
+  })
+
+  $routeProvider.when('/albums',{
+    templateUrl: 'views/albums.html'
+  })
+
+}]);
+
 
 myApp.controller('myMainController', ['$scope', '$http', function($scope, $http){
   $http.get('data/data.json').then(function(response){
-    $scope.musicPlay = response.data;
+    $scope.musicArtist = response.data;
+    console.log(response.data);
+      //  $scope.musicAlbums = [];
+        //Assign JSON obj to results to repeat through and display data
+        //angular.forEach(response.data, function(value, key){
+          //musicAlbums = value.albums;
+          //console.log(musicAlbums);
+      //  });
+
   });
 
 }]);
